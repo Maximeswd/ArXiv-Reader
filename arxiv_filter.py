0400a93ab6d308ed55c1822467756caab11838cd
Maxime Dassen <maximedassen@yahoo.com>
2025-11-10 10:41:21 +0100

    updates

--- arxiv_filter.py
+++ arxiv_filter.py
@@ -1,29 +1,45 @@
+# Updated arxiv_filter.py (Improved Logic)
+
 import pandas as pd
 from rich.console import Console
 from rich.table import Table
 from rich.style import Style
 from rich import box
+import argparse
 
 from util import add_to_table, get_until
 
 console = Console()
 
+parser = argparse.ArgumentParser(description="Filter arXiv daily emails based on keywords and authors.")
+parser.add_argument('-k', '--keyword', nargs='+', help='A list of keywords to search for in title and abstract.')
+parser.add_argument('-a', '--author', nargs='+', help='A list of authors to search for.')
+args = parser.parse_args()
+
 with console.status('Reading papers...', spinner='monkey'):
-    # --- SETUP ---
-    # read keywords and authors from keywords.txt and authors.txt
-    with open('keywords.txt', 'r') as file:
-        keywords = file.read().splitlines()
-        keywords = [keyword for keyword in keywords if len(keyword) > 2]
+    # If the user provides any command-line argument (-k or -a), we will ONLY use what they provide.
+    # The .txt files will be ignored.
+    if args.keyword or args.author:
+        console.print("[yellow]Command-line arguments detected. Ignoring .txt files.[/yellow]")
+        # Use the provided keyword(s), or an empty list if none were given
+        keywords = args.keyword if args.keyword else []
+        # Use the provided author(s), or an empty list if none were given
+        authors = args.author if args.author else []
+    
+    # If NO command-line arguments are given, fall back to the .txt files.
+    else:
+        console.print("[yellow]No command-line arguments. Using keywords.txt and authors.txt.[/yellow]")
+        with open('keywords.txt', 'r') as file:
+            keywords = file.read().splitlines()
+            keywords = [keyword for keyword in keywords if len(keyword) > 2]
 
-    with open('authors.txt', 'r') as file:
-        authors = file.read().splitlines()
-        authors = [author for author in authors if len(author) > 2]
-
-    # read lines from mail_text.txt
+        with open('authors.txt', 'r') as file:
+            authors = file.read().splitlines()
+            authors = [author for author in authors if len(author) > 2]
+    
     with open('mail_text.txt', 'r') as file:
         lines = file.readlines()
 
-    # --- PROCESS ---
     results = {
         'title': [],
         'authors': [],
@@ -44,26 +60,28 @@ with console.status('Reading papers...', spinner='monkey'):
         elif line.startswith('Categories:'):
             results['abstract'].append(get_until(i, lines, '-----------------------'))
         elif line.startswith("%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%--%%"):
-            break # only until the updates
+            break
 
     df = pd.DataFrame(results)
 
-    # --- OUTPUT ---
     table = Table(box=box.HORIZONTALS, show_lines=False, show_header=False)
     keyword_style = Style(color="red", bold=True)
     table.add_column(max_width=15, justify='center', style=keyword_style)
     table.add_column()
 
-    for keyword in keywords:
-        table.add_row(keyword)
-        match = df[df['title'].str.contains(keyword, case=False) | df['abstract'].str.contains(keyword, case=False)]
-        table = add_to_table(match, table, keyword)
-        table.add_section()
+    if not keywords and not authors:
+        console.print("[bold red]No keywords or authors to search for.[/bold red]")
+    else:
+        for keyword in keywords:
+            table.add_row(keyword)
+            match = df[df['title'].str.contains(keyword, case=False) | df['abstract'].str.contains(keyword, case=False)]
+            table = add_to_table(match, table, keyword)
+            table.add_section()
 
-    for author in authors:
-        table.add_row(author)
-        match = df[df['authors'].str.contains(author, case=False)]
-        table = add_to_table(match, table, author)
-        table.add_section()
+        for author in authors:
+            table.add_row(author)
+            match = df[df['authors'].str.contains(author, case=False)]
+            table = add_to_table(match, table, author)
+            table.add_section()
 
-console.print(table)
+        console.print(table)
